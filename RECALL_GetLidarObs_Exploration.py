fa1026499de506fd14555277f5943e7d7d3f3061
ConlinM <conlinm@ufl.edu>
2019-04-23 17:44:33 -0400

    Worked on lidar download function

--- RECALL_GetLidarObs_Exploration.py
+++ RECALL_GetLidarObs_Exploration.py
@@ -9,6 +9,8 @@ Created on Mon Apr  8 11:22:40 2019
 # Download specified file from FTP to local file #
 import ftplib
 import sys
+import requests
+import pandas as pd
 
 
 # Function to draw progress bar while code is working #
@@ -35,8 +37,8 @@ ftp.cwd('/pub/DigitalCoast/lidar2_z/geoid12b/data/')
 IDs = ftp.nlst()
 
 
-# Loop through each dataset to see if it captures where the camera can see #
-appropID = list()
+# Loop through each dataset to see if it captures where the camera can see. Store the datasets which do. #
+appropID = list() # Initiate list of IDs which contain the camera location #
 i = 0
 for ID in IDs:
     i = i+1
@@ -45,50 +47,65 @@ for ID in IDs:
     
     # Get the bounds of all of the regions in the current set #
     ftp.cwd('/pub/DigitalCoast/lidar2_z/geoid12b/data/'+str(ID))  
-     
+    
     # Find the minmax csv file which shows the min and max extents of each tile within the current dataset #
     files = ftp.nlst()
     fileWant = str([s for s in files if "minmax" in s])
-    fileWant = fileWant[2:len(fileWant)-2]
-    # If there is more than one csv file, take only the first #
-    if len(fileWant.split()) == 2:
-        fileWant = fileWant.split()[1]
     
-    # Save the minmax file locally #
-    gfile = open('minmax.csv','wb') # Create the local file #
-    ftp.retrbinary('RETR '+fileWant,gfile.write) # Copy the contents of the file on FTP into the local file #
-    gfile.close()
-    
-    # See if the location of the camera is contained within any of the tiles in this dataset #
-    with open('minmax.csv') as infile:
-        next(infile)
-        for line in infile:
-            if float(line.split()[1][0:7]) <= cameraLoc_lon <= float(line.split()[2][0:7]) and float(line.split()[3][0:7])<= cameraLoc_lat <= float(line.split()[4][0:7]):
-                appropID.append(ID)
-        
-
-# Display the names of the sets that contain the data and choose one # 
-
-
-
-
-
-
-
-
-
-
-
-
-ftp.close() 
-
-
-
-
-
-
-      
+    if len(fileWant>2):
+        # Get the file name and save it. We need to get rid of the ' or " in the name. Sometimes this means we need to get rid of the first 2 characters, sometimes the first 3 #
+        if len(fileWant.split()) == 2:
+            fileWant = '['+fileWant.split()[1]
+            fileWant = fileWant[2:len(fileWant)-2]
+            # Save the file locally #
+            gfile = open('minmax.csv','wb') # Create the local file #
+            ftp.retrbinary('RETR '+fileWant,gfile.write) # Copy the contents of the file on FTP into the local file #
+            gfile.close() # Close the remote file #
     
     
+        # See if the location of the camera is contained within any of the tiles in this dataset. If it is, save the ID #
+        tiles = list()
+        with open('minmax.csv') as infile:
+            next(infile)
+            for line in infile:
+                if float(line.split()[1][0:7]) <= cameraLoc_lon <= float(line.split()[2][0:7]) and float(line.split()[3][0:7])<= cameraLoc_lat <= float(line.split()[4][0:7]):
+                    tiles.append(line)
+
+        if len(tiles)>0:       
+            appropID.append(ID)
+
+
+########### Select the desired dataset by linking IDs to names ############
+# Get the data tabel on NOAAs website #
+url = 'https://coast.noaa.gov/htdata/lidar1_z/'
+html = requests.get(url).content
+df_list = pd.read_html(html)
+dataTable = df_list[-1]
+# Make a list of all IDs and names #   
+IDlist = dataTable.loc[:,'ID #']
+nameList = dataTable.loc[:,'Dataset Name']    
+ # Find the indicies in the data table that match the appropriate IDs # 
+appropIDNums = list(map(int,appropID))  
+matchingTableRows = [i for i, x in enumerate(IDlist) for j,y in enumerate(appropIDNums) if x==y] # Get indicies of matching IDs in the dataTable
+# Create a new data frame with data for the appropriate IDs #
+matchingTable = pd.DataFrame(columns=['ID','Year Collected','Name'])
+matchingTable.loc[:,'ID'] = IDlist[matchingTableRows]
+matchingTable.loc[:,'Year Collected'] = dataTable.loc[:,'Year'][matchingTableRows]
+matchingTable.loc[:,'Name'] = nameList[matchingTableRows]
+# Display the data frame and wait for input of dataset to use #
+print(matchingTable)
+matchingTable_IDUse = input('Select the dataset which you would like to use. Specify as the ro ID in the left-most column: ')
+
+
+########### Download the desired dataset #####################
+# Get the chosen ID #
+IDToDownload = matchingTable.loc[int(matchingTable_IDUse),'ID']
+# Go back to the FTP and download the chosen ID #
+ftp = ftplib.FTP('ftp.coast.noaa.gov','anonymous','conlinm@ufl.edu')
+ftp.cwd('/pub/DigitalCoast/lidar2_z/geoid12b/data/'+str(IDToDownload))
+
+
+
+
     
 
